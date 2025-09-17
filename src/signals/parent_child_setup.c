cc6541dd7abe7f4ba18d8af3f05f9cfea463debe
niwotgirl <alexandraabelkis@gmail.com>
2025-09-17 15:42:59 +0200

    more norm stuff for signals and utils

--- src/signals/parent_child_setup.c
+++ src/signals/parent_child_setup.c
@@ -1,21 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parent_child_setup.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aabelkis <aabelkis@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/17 15:29:42 by aabelkis          #+#    #+#             */
+/*   Updated: 2025/09/17 15:41:55 by aabelkis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../minishell.h"
 
-/*here I set up sigaction struct so default actions occur instead of what we had in main and then execute SIGINT and SIGQUIT signals in child*/
-void child_signal_setup(void)
+/*here I set up sigaction struct so default actions occur instead of what we 
+	had in main and then execute SIGINT and SIGQUIT signals in child*/
+void	child_signal_setup(void)
 {
-	struct sigaction sa;
+	struct sigaction	sa;
 
-    sa.sa_handler = SIG_DFL;
+	sa.sa_handler = SIG_DFL;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
-
 	if (sigaction(SIGINT, &sa, NULL) == -1)
 	{
 		write(2, "error with sigint_handler \n", 26);
 		exit(1);
 	}
-
 	if (sigaction(SIGQUIT, &sa, NULL) == -1)
 	{
 		write(2, "error with sigquit \n", 19);
@@ -23,21 +33,20 @@ void child_signal_setup(void)
 	}
 }
 
-/*here I set up sigaction struct so that signals are ignored instead of what we had in main (for parent process)*/
-void parent_signal_setup (void)
+/*here I set up sigaction struct so that signals are ignored instead 
+	of what we had in main (for parent process)*/
+void	parent_signal_setup(void)
 {
-    struct sigaction sa;
+	struct sigaction	sa;
 
-    sa.sa_handler = SIG_IGN;
+	sa.sa_handler = SIG_IGN;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
-
 	if (sigaction(SIGINT, &sa, NULL) == -1)
 	{
 		write(2, "error with sigint_handler \n", 26);
 		exit(1);
 	}
-
 	if (sigaction(SIGQUIT, &sa, NULL) == -1)
 	{
 		write(2, "error with sigquit \n", 19);
